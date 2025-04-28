0d9cf1fc40802f78bdf8e354bd87b762f4d0d464
Aarush Aggarwal <76877384+Aarushaggarwal-theone@users.noreply.github.com>
2025-04-28 14:22:01 +0530

    added article and tailwind config

---
+++ app/tailwind.config.mjs
@@ -0,0 +1,8 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    // ... other configurations
+    plugins: [
+      require('@tailwindcss/typography'),
+    ],
+  };
